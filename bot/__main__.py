9e16eed4525cbdd3a2e638f7288c8d9be88852b9
takahashimaaya <105087697+takahashimaaya@users.noreply.github.com>
2022-05-07 06:19:35 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -12,7 +12,7 @@ from sys import executable
 from telegram import ParseMode, InlineKeyboardMarkup
 from telegram.ext import CommandHandler
 
-from wserver import start_server_async
+## from wserver import start_server_async
 from bot import HEROKU_API_KEY, HEROKU_APP_NAME, bot, app, dispatcher, updater, botStartTime, IGNORE_PENDING_REQUESTS, IS_VPS, PORT, alive, web, OWNER_ID, AUTHORIZED_CHATS, LOGGER, Interval, nox, rss_session, a2c
 from .helper.ext_utils.fs_utils import start_cleanup, clean_all, exit_clean_up
 from .helper.telegram_helper.bot_commands import BotCommands
